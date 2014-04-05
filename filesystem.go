f6dfa245eb8998702fea38cef7a4e4978ad01d9a
Matt Soucy <msoucy@csh.rit.edu>
2014-04-05 19:04:14 -0400

    Now creates a complete recursive list and uses fsnotify on it

--- filesystem.go
+++ filesystem.go
@@ -1,24 +1,41 @@
 package main
 
 import (
-	"os"
-	"log"
 	"github.com/howeyc/fsnotify"
+	"log"
+	"os"
+	"path/filepath"
 )
 
-func ls(filename string) []string {
-	f, err := os.OpenFile(filename, os.O_RDONLY, 0)
-	if f == nil {
-		log.Fatalf("ls: cannot access %s: %s\n", filename, err)
-	}
-	defer f.Close()
+type MediaData struct {
+	name      string
+	directory bool
+	id        uint
+}
 
-	files, err := f.Readdirnames(-1)
-	if files == nil {
-		log.Fatalf("ls: could not get files in %s: %s\n", filename, err)
-	}
+var WatchChanges uint32 = fsnotify.FSN_DELETE | fsnotify.FSN_RENAME | fsnotify.FSN_CREATE
 
-	return files
+func ListFiles(filename string, watcher *fsnotify.Watcher) []MediaData {
+
+	media := make([]MediaData, 0)
+	var i uint
+	filepath.Walk(filename, func(path string, info os.FileInfo, err error) error {
+		fname, err := filepath.Abs(path)
+		if err != nil {
+			return err
+		}
+		isdir := (info.Mode() & os.ModeDir) != 0
+		if isdir {
+			watcher.WatchFlags(path, WatchChanges)
+			log.Println("Now tracking folder", path)
+		}
+		media = append(media, MediaData{
+			fname, isdir, i,
+		})
+		i++
+		return nil
+	})
+	return media
 }
 
 func ProcessFilesystem(dirs []string) {
@@ -26,7 +43,6 @@ func ProcessFilesystem(dirs []string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 
 	done := make(chan bool)
 
@@ -45,10 +61,9 @@ func ProcessFilesystem(dirs []string) {
 		}
 	}()
 
-	var flags uint32 = fsnotify.FSN_DELETE | fsnotify.FSN_RENAME | fsnotify.FSN_CREATE
 	for _, fold := range dirs {
-		err = watcher.WatchFlags(fold, flags)
-		log.Println("Found items: ", ls(fold))
+		err = watcher.WatchFlags(fold, WatchChanges)
+		log.Println("Found items: ", ListFiles(fold, watcher))
 		if err != nil {
 			log.Fatal(err)
 		}
