379c5819dedea0e3e10e5e6537d5358b1aee3137
Robert Glossop <robgssp@gmail.com>
2014-04-06 11:54:00 -0400

    web ui works

--- controller.go
+++ controller.go
@@ -2,11 +2,12 @@ package main
 
 import (
 //	"os"
+	"io"
 	"fmt"
 	"net"
+	"bytes"
 	"net/http"
-//	"bufio"
-//	"html/template"
+	"html/template"
 	"encoding/json"
 )
 
@@ -35,21 +36,79 @@ func main() {
 	httpListen()
 }
 
+var srvs []*Server = make([]*Server, 0)
+
 // HTTP Frontend stuff
 
 func httpListen() {
 	http.HandleFunc("/", mainMenu)
 
-
-
 	http.ListenAndServe(":3424", nil)
 }
 
 func mainMenu(w http.ResponseWriter, r *http.Request) {
-	t, err = template.New("foo").Parse(`<html><body><h1>Bootleg</h1><ul>{{.}}</ul></body></html>`)
+	t, err := template.New("foo").Parse(`<html><body><h1>Bootleg</h1>{{.}}</body></html>`)
+	if err != nil { panic(err); }
 
+	buf := bytes.Buffer{}
+
+	type ServerArg struct {
+		Name string
+		Body template.HTML
+	}
+
+	for _, srv := range srvs {
+		t1, err1 := template.New("bar").Parse(`<h2>{{.Name}}</h2><ul>{{.Body}}</ul>`)
+		if err1 != nil { panic(err1); }
+		
+		buf1 := bytes.Buffer{}
+		formatDir(srv.files, &buf1)
+		t1.Execute(&buf, ServerArg{srv.name, template.HTML(buf1.String())})
+	}
+
+	t.Execute(w, template.HTML(buf.String()))
 }
+
+func formatDir(d Dir, b io.Writer) {
+	type DirArg struct {
+		Name string
+		Files template.HTML
+	}
+
+	t, err := template.New("foo").Parse(`<li>{{.Name}}</li><ul>{{.Files}}</ul></li>`)
+	if err != nil {
+		fmt.Printf("formatDir err: %s\n", err)
+		panic(err)
+	}
+
+	buf := bytes.Buffer{}
+
+	for _, fd := range d.children {
+		switch fd.(type) {
+		case Dir:
+			formatDir(fd.(Dir), &buf)
+		case File:
+			formatFile(fd.(File), &buf)
+		}
+	}
+
+	t.Execute(b, DirArg{d.name, template.HTML(buf.String())})
+}
+
+func formatFile(f File, b io.Writer) {
+	type FileArg struct {
+		Id int
+		Name string
+	}
 	
+	t, err := template.New("foo").Parse(`<li><a href="/id/{{.Id}}">{{.Name}}</a></li>`)
+	if err != nil {
+		panic(err)
+	}
+
+	t.Execute(b, FileArg{f.id, f.name})
+}
+
 // Server-manager stuff
 
 func srvListen() {
@@ -70,9 +129,6 @@ func srvListen() {
 }
 
 func server(c net.Conn) {
-	// rd := bufio.NewReader(c)
-	// wr := bufio.NewWriter(c)
-
 	dec := json.NewDecoder(c)
 
 	defer func() {
@@ -82,7 +138,9 @@ func server(c net.Conn) {
 		}
 	}()
 	
-	srv := readServer(c)
+	srv := readServer(c, dec)
+
+	srvs = append(srvs, &srv)
 	fmt.Printf("Received %s\n", srv)
 
 	var res map[string]interface{}
@@ -100,7 +158,7 @@ func server(c net.Conn) {
 	}
 }
 
-func readServer(c net.Conn, dec json.Decoder) Server {
+func readServer(c net.Conn, dec *json.Decoder) Server {
 	var res map[string]interface{}
 
 	if err := dec.Decode(&res); err != nil {
